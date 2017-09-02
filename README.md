a65e9795455f27e939f02521881b83a988c951f4
tomekceszke <tomekceszke@users.noreply.github.com>
2017-09-03 01:22:10 +0200

    some improvements

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# gas-consumption-forecast
+# Gas consuption forecast
 Attempt to use Machine Learning to predict natural gas consumption for domestic needs like heating and cooking.
-As a training data I use collected historical values.
+As a training data I use my historical values. 
 
+*Intentionally written in pure Matlab languge using only basic mathematics operations.*
+
+* Prediction model: polynomial regression
+* Optimization algorithm: [normal equation](https://en.wikipedia.org/wiki/Linear_least_squares_(mathematics))
 
 ![sample](doc/img/training_data_anf_hypothesis.PNG)
 
-TBD...
+### Training efficiency
+![red_sample](doc/img/predicted_real.png)
+
+### Requirements
+* [Octave](https://www.gnu.org/software/octave)
--- consumption-forecast.m
+++ consumption-forecast.m
@@ -1,97 +1,115 @@
+% Predict consumption of gas for domestic needs
+% model: polynomial regression
+% alogrithm: normal equation
+% 
+% Author: Tomasz Ceszke
+
 clear ; close all; clc
 
-% get training data
+% get training & test data
 fprintf('Loading data ...\n');
 data = load('data/data_month.txt');
-X = data(:, 1); y = data(:, 2);
-m = length(y);
-fprintf('Loaded %d samples\n',m);
+data_train = data(1:end-12,:);
+data_test = sortrows(data(end-11:end,:));
+
+fprintf('Loaded %d samples:\n',length(data));
+X_train = data_train(:,1);
+y_train = data_train(:,2);
+m = length(y_train);
+fprintf("  training %d samples\n",m);
+X_test = data_test(:,1);
+y_test = data_test(:,2);
+fprintf("  test %d samples\n",length(y_test));
 
 % plot training data
 figure;
-plot(X, y, 'rx', 'MarkerSize', 10);
+plot(X_train, y_train, 'rx', 'MarkerSize', 10);
 ylabel('Consumption [m3/month]'); 
 xlabel('Month'); 
-fprintf('Program paused. Press any key to continue.\n');
+fprintf('\nProgram paused. Press any key to continue.\n\n');
 pause;
 
 % --------------------- linear regression - linear model -----------------------
-X = [ones(m, 1) X];
-theta = pinv(X'*X)*X'*y;
+X_train = [ones(m, 1) X_train]; # added interceptor
+theta = pinv(X_train'*X_train)*X_train'*y_train; % normal equation
 fprintf('Hypothesis function: h(x)=%.2f+%.2fx \n',theta(1),theta(2));
 
 hold on; 
-%plot(X(:,2), X*theta, '-')
+%plot(X_train(:,2), X_train*theta, '-')
 x_plot = [1:.1:12]';
 %y_plot = [ones(rows(x_plot),1) x_plot]*theta; 
 linear = plot(x_plot, theta(1)+theta(2)*x_plot, '-');
 %plot(x_plot, y_plot, '-')
 legend('Training data', 'Linear regression')
-fprintf('Program paused. Press any key to continue.\n');
+fprintf('\nProgram paused. Press any key to continue.\n\n');
 pause;
 %set(linear,'Visible','off');
 
 % --------------------- linear regression - quadratic model --------------------
-X = [X X(:,2).^2];
-theta = pinv(X'*X)*X'*y;
+X_train = [X_train X_train(:,2).^2];
+theta = pinv(X_train'*X_train)*X_train'*y_train;
 fprintf('Hypothesis function: h(x)=%.2f+%.2fx+%.2fx^2 \n',theta(1),theta(2), theta(3));
 
 %ezplot(@(x) theta(1)+theta(2)*x+theta(3)*x.^2);
-%plot(X(:,2), X*theta, '-', 'color','g')
+%plot(X_train(:,2), X_train*theta, '-', 'color','g')
 %y_plot = [ones(rows(x_plot),1) x_plot x_plot.^2]*theta; 
 %plot(x_plot, y_plot,'-','color','g')
 quadratic = plot(x_plot, theta(1)+theta(2)*x_plot+theta(3)*x_plot.^2, '-');
 legend('Training data', 'Linear', 'Quadratic')
-fprintf('Program paused. Press any key to continue.\n');
+fprintf('\nProgram paused. Press any key to continue.\n\n');
 pause;
 %set(quadratic,'Visible','off');
 
 % ---------------------linear regression - cubic model -------------------------
-X = [X X(:,2).^3];
-theta = pinv(X'*X)*X'*y;
+X_train = [X_train X_train(:,2).^3];
+theta = pinv(X_train'*X_train)*X_train'*y_train;
 fprintf('Hypothesis function: h(x)=%.2f+%.2fx+%.2fx^2+%.2fx^3 \n',theta(1),theta(2), theta(3), theta(4));
-%plot(X(:,2), X*theta, '-', "color","k")
+%plot(X_train(:,2), X_train*theta, '-', "color","k")
 %y_plot = [ones(rows(x_plot),1) x_plot x_plot.^2 x_plot.^3]*theta; 
 %plot(x_plot, y_plot,'-','color','k')
 cubic = plot(x_plot, theta(1)+theta(2)*x_plot+theta(3)*x_plot.^2+theta(4)*x_plot.^3, '-'); 
 legend('Training data', 'Linear', 'Quadratic', 'Cubic')
-fprintf('Program paused. Press any key to continue.\n');
+fprintf('\nProgram paused. Press any key to continue.\n\n');
 pause;
 %set(cubic,'Visible','off');
 
 % --------------------- linear regression - quartic model ----------------------
-X = [X X(:,2).^4];
-theta = pinv(X'*X)*X'*y;
+X_train = [X_train X_train(:,2).^4];
+theta = pinv(X_train'*X_train)*X_train'*y_train;
 fprintf('Hypothesis function: h(x)=%.2f+%.2fx+%.2fx^2+%.2fx^3+%.2fx^4 \n',theta(1),theta(2), theta(3), theta(4), theta(5));
-%plot(X(:,2), X*theta, '-', "color","k")
+%plot(X_train(:,2), X_train*theta, '-', "color","k")
 %y_plot = [ones(rows(x_plot),1) x_plot x_plot.^2 x_plot.^3 x_plot.^4]*theta; 
 %plot(x_plot, y_plot,'-','color','m')
 quartic = plot(x_plot, theta(1)+theta(2)*x_plot+theta(3)*x_plot.^2+theta(4)*x_plot.^3+theta(5)*x_plot.^4, '-');
 legend('Training data', 'Linear', 'Quadratic', 'Cubic', 'Quartic')
-fprintf('Program paused. Press any key to continue.\n');
+fprintf('\nProgram paused. Press any key to continue.\n\n');
 pause;
 %set(quartic,'Visible','off');
 
 % --------------------- linear regression - quintic model ----------------------
-X = [X X(:,2).^5];
-theta = pinv(X'*X)*X'*y;
+X_train = [X_train X_train(:,2).^5];
+theta = pinv(X_train'*X_train)*X_train'*y_train;
 fprintf('Hypothesis function: h(x)=%.2f+%.2fx+%.2fx^2+%.2fx^3+%.2fx^4+%.2fx^5 \n',theta(1), theta(2), theta(3), theta(4), theta(5), theta(6));
-%plot(X(:,2), X*theta, '-', "color","k")
+%plot(X_train(:,2), X_train*theta, '-', "color","k")
 %y_plot = [ones(rows(x_plot),1) x_plot x_plot.^2 x_plot.^3 x_plot.^4 x_plot.^5]*theta; 
 %plot(x_plot, y_plot,'-','color','c')
 quintic = plot(x_plot, theta(1)+theta(2)*x_plot+theta(3)*x_plot.^2+theta(4)*x_plot.^3+theta(5)*x_plot.^4+theta(6)*x_plot.^5, '-');
 legend('Training data', 'Linear', 'Quadratic', 'Cubic', 'Quartic', 'Quintic')
-%fprintf('Program paused. Press any key to continue.\n');
+%fprintf('\nProgram paused. Press any key to continue.\n\n');
 hold off 
 
 % predictions %
+function y = predict5(X, theta)
+  y = theta(1)+theta(2)*X+theta(3)*X.^2+theta(4)*X.^3+theta(5)*X.^4+theta(6)*X.^5;
+end  
 
-predict_months = [1:1:12]';
-predictions = theta(1)+theta(2)*predict_months+theta(3)*predict_months.^2+theta(4)*predict_months.^3+theta(5)*predict_months.^4+theta(6)*predict_months.^5;
+y_pred = predict5(X_test, theta)
 
-fprintf('Predictions:\n')
-[predict_months predictions]
+fprintf('\nPredictions:\n')
+[X_test y_pred]
 
-
+plot(X_test, y_pred, '-'); hold on
+plot(X_test, y_test, '-'); hold off
+legend('Predicted','Real')
 
 
